1eb28b06d801cb2c2982d918b77f6e9d94b00a03
Florian <florian.martin63000@gmail.com>
2020-12-13 19:31:26 +0100

    Initial commit

---
+++ src/test/java/com/reactive/mongodb/router/function/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.mongodb.router.function.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
